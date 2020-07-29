2213af51ed042f12a658b1af82f3fd7e0c2ec847
Maximiliano Geywitz <mr_asis@hotmail.com>
2020-07-29 11:29:05 -0400

    docs agregado

---
+++ docs/precache-manifest.d4a167ff8ad0a3d2b0a20a9b270862a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d700489da6efd188aca6a864905968aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a608286ce21f41d97e3",
+    "url": "/static/css/main.8875868d.chunk.css"
+  },
+  {
+    "revision": "7de69849bc80a5964983",
+    "url": "/static/js/2.141553da.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.141553da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a608286ce21f41d97e3",
+    "url": "/static/js/main.9cbe3c92.chunk.js"
+  },
+  {
+    "revision": "4d3c5aad2ee9da455662",
+    "url": "/static/js/runtime-main.a4f11275.js"
+  }
+]);
